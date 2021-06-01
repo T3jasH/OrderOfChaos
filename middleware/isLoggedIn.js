48d9d55a7053c62d47c22597e73349f89a23a180
Tushar Maroo <maroo.tushar@gmail.com>
2021-06-01 18:58:09 +0530

    bjhb

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,6 +1,16 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
+const getIP = (ip) =>{
+    var ct=0;
+    for(i=0; i<ip.length; i++)
+    {
+        if(ip[i]==':')ct++;
+        if(ct===4) return ip.substr(0,i);
+    }
+    return ip;
+}
+
 module.exports = async function (req, res, next) {
     // Get the token fron header
     const token = req.header("x-auth-token");
@@ -20,10 +30,9 @@ module.exports = async function (req, res, next) {
         req.user = decoded.user;
         const user = await User.findById(req.user.id).select("ipaddress");
         // console.log(user.ipaddress);
-        if(user.ipaddress && user.ipaddress!==req.ipInfo.ip)
+        if(user.ipaddress && getIP(user.ipaddress)!==getIP(req.ipInfo.ip))
         {
-            console.log(`IpError: ${user.ipaddress} != ${req.ipInfo.ip}`);
-            console.log(req.ipInfo);
+            console.log(`IpError: ${getIP(user.ipaddress)} != ${getIP(req.ipInfo.ip)}`);
             return res.status(400).json({
                 success: false,
                 msg: "You can log in from one computer only.",
