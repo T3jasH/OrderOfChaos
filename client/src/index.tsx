6324ef80e6b2886134e15d7a81a611eaecb3ab2c
Tushar Maroo <maroo.tushar@gmail.com>
2021-06-05 14:44:33 +0530

    captcha final

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,17 +7,9 @@ import { BrowserRouter as Router } from "react-router-dom"
 import AuthProvider from "./context/AuthContext"
 import QuestionListProvider from "./context/QuestionContext"
 import PlayerProvider from "./context/PlayerContext"
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
-// import { GoogleReCaptchaProvider } from '../src/google-recaptcha-provider';
-// import { GoogleRecaptchaExample } from './google-recaptcha-example';
-// import { WithGoogleRecaptchaExample } from './with-google-recaptcha-example';
-
 
 ReactDOM.render(
     <Router>
-        <GoogleReCaptchaProvider useRecaptchaNet reCaptchaKey = '6LcIPhIbAAAAAG4Rn8C5IkFd5pkCTsjuHBkHG2iV'
-            scriptProps={{ async: true, defer: true, appendTo: 'body' }} 
-        >
         <AuthProvider>
             <PlayerProvider>
                 <QuestionListProvider>
@@ -25,7 +17,6 @@ ReactDOM.render(
                 </QuestionListProvider>
             </PlayerProvider>
         </AuthProvider>
-        </GoogleReCaptchaProvider>
     </Router>,
     document.getElementById("root")
 )
